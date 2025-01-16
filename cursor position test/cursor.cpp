cfbf9b25b2b8778ca3f073bb938d2de8dd136848
studentColl <ggxaoc@gmail.com>
2025-01-16 10:59:22 +0300

    Cursor position cool experiment update

--- cursor position test/cursor.cpp
+++ cursor position test/cursor.cpp
@@ -5,13 +5,20 @@ HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 
 int main() {
 	POINT CursorPos;
-	COORD CursorCoord;
+	COORD CursorCoord, OldCoord = { 0, 0 };
 	while (1) {
+		system("cls");
 		GetCursorPos(&CursorPos);
 		CursorCoord.X = CursorPos.x / 8;
 		CursorCoord.Y = CursorPos.y / 16;
+
+		SetConsoleCursorPosition(console, OldCoord);
+		std::cout << "\033[40m  ";
+		OldCoord = CursorCoord;
 		//std::cout << CursorPos.x << CursorPos.y << ' ';
 		SetConsoleCursorPosition(console, CursorCoord);
+		std::cout << "\033[107m  \033[40m";
+		Sleep(5);
 	}
 
 	return 0;
