caeb1a86469074dd431031287d5dafea6f502fc2
studentColl <ggxaoc@gmail.com>
2025-01-16 11:00:20 +0300

    Cursor position cool update

--- cursor position test/cursor.cpp
+++ cursor position test/cursor.cpp
@@ -7,7 +7,6 @@ int main() {
 	POINT CursorPos;
 	COORD CursorCoord, OldCoord = { 0, 0 };
 	while (1) {
-		system("cls");
 		GetCursorPos(&CursorPos);
 		CursorCoord.X = CursorPos.x / 8;
 		CursorCoord.Y = CursorPos.y / 16;
@@ -18,7 +17,6 @@ int main() {
 		//std::cout << CursorPos.x << CursorPos.y << ' ';
 		SetConsoleCursorPosition(console, CursorCoord);
 		std::cout << "\033[107m  \033[40m";
-		Sleep(5);
 	}
 
 	return 0;
